7db578bf976abd2bc25afb290f8dc6d227f77cd7
Samuel <samuelknobel@hotmail.com>
2020-12-04 12:33:26 +0100

    Day 1 Part2

--- Day01.py
+++ Day01.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-
+# PART 1
 data = pd.read_table("Data/inputDay1.txt",header=None)
 bigger = data[0][data[0]>1010]
 smaller = data[0][data[0]<=1010]
 
 solution = [(x * y) for x in bigger for y in smaller if x+y ==2020]
+print(solution)
+
+# PART 2
+solution = [(x , y, z) for z in data[0] for x in data[0] for y in data[0] if x+y+z ==2020 ]
+print(solution)
+solution = [(x * y* z) for z in data[0] for x in data[0] for y in data[0] if x+y+z ==2020 and x!=y and y!= z and z!= x ]
 print(solution)
