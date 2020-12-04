1961ecbe089b734bdd9b7b54b596402fe16dbeb6
Samuel <samuelknobel@hotmail.com>
2020-12-04 12:26:26 +0100

    Day 1 finished clean up

--- Day01.py
+++ Day01.py
@@ -7,13 +7,3 @@ smaller = data[0][data[0]<=1010]
 
 solution = [(x * y) for x in bigger for y in smaller if x+y ==2020]
 print(solution)
-
-
-#for i in bigger:
-#    for j in smaller:
-#        print ( i + j)
-#        if i + j == 2020:
-#            print(i*j)
-#            break
-#        else:
-#            continue
